0245f2ed163a06575c3eeacd8c778c6ee851b05e
Otis <otisvickersgraver@gmail.com>
2019-09-21 18:25:10 +0100

    initial commit

---
+++ src/FirstApp.kt
@@ -0,0 +1,12 @@
+
+fun main(args:Array<String>){
+
+    println("Hi Otis, \n")
+    println("Welcome to Kotlin")
+
+    println("Hi Otis, ")
+    println("Welcome to Kotlin")
+
+    print("Hi Otis, ")
+    print("Welcome to Kotlin")
+}
