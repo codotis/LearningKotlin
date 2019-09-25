1931b8b945634bd82aace6b3076dfe1aec71c39a
Otis <otisvickersgraver@gmail.com>
2019-09-25 17:51:52 +0100

    included variable for calendar year

--- src/FindMyAge.kt
+++ src/FindMyAge.kt
@@ -1,11 +1,12 @@
-
+import java.util.*
 
 fun main(args:Array<String>) {
 
     print("Please enter your year of birth:")
     var birthYear:Int = readLine()!!.toInt()
 
-    var age = 2019 - birthYear
+    var currentYear = Calendar.getInstance().get(Calendar.YEAR) //reads from computers calendar. get instance of that calendar then get calendar year from that instance
+    var age = currentYear - birthYear
 
     print("Your age is: $age")
 
