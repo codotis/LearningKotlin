4bed19b649f82094bd368af09e0c7a13ff1a7639
Otis <otisvickersgraver@gmail.com>
2019-10-06 22:01:04 +0100

    basic if statements

--- src/SimpleIfStatement.kt
+++ src/SimpleIfStatement.kt
@@ -1,3 +1,27 @@
 fun main(args:Array<String>) {
 
+    println("Enter the amount of trainers you own:")
+    var trainers = readLine()!!.toDouble()
+
+    if (trainers >= 50) {
+        println("Wow! You're a real sneakerhead, aren't you? Considered opening up a store?")
+    }
+
+    if (trainers >= 25 && trainers < 50) {
+        println("That's an unhealthy amount of trainers. One for every day in the month, eh?")
+    }
+
+    if (trainers >= 10 && trainers < 25) {
+        println("Steady on, I see where this is going!")
+    }
+
+    if (trainers >= 5 && trainers < 10) {
+        println("Not a fanatic, but you like to have nice footwear")
+    }
+
+    if (trainers >= 0 && trainers < 5) {
+        println("Small but sweet. Unless you don't own any, then I guess you prefer a different style")
+    }
+
+
 }
